3cd4db8d54afeb4e5f6c59b4fa72efa7933c632b
becaale <becaale@hotmail.com>
2023-01-24 18:23:19 +0100

    TIPs

--- Ejercicios/05-MinStack/MinStack.js
+++ Ejercicios/05-MinStack/MinStack.js
@@ -1,14 +1,73 @@
 class Node {
-  // Your code here:
-
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+    this.min = null;
+  }
 }
 
 class MinStack {
-  // Your code here:
+  constructor() {
+    this.head = null;
 
+    this.push = (value) => {
+      const newNode = new Node(value);
+      newNode.next = this.head;
+      this.head = newNode;
+      if (!this.head.next) this.head.min = value;
+      else this.head.min = value < this.head.next.min ? value : this.head.next.min;
+    };
+
+    this.pop = () => {
+      if (!this.head) return "Stack vacío";
+      const value = this.head.value;
+      this.head = this.head.next;
+      return value;
+    };
+
+    this.peek = () => (this.head ? this.head.value : "Stack vacío");
+
+    this.min = () => {
+      // if (!this.head) return "Stack vacío";
+      // let current = this.head;
+      // let min = current.value;
+
+      // while (current.next) {
+      //   current = current.next;
+      //   if (current.value < min) min = current.value;
+      // }
+      // return min;
+      return this.head.min;
+    };
+  }
 }
+
+// push(value) : añadir el elemento, value, al stack.
+// pop() : sacar un elemento del stack.
+// min() : obtener el elemento con el valor mínimo.
+// peek(): obtener el elemento que está el en top del Stack
 
 module.exports = {
   Node,
-  MinStack
-}
+  MinStack,
+};
+
+const miNodo = new Node(5);
+console.log(miNodo);
+
+const miStack = new MinStack();
+miStack.push(5);
+miStack.push(10);
+miStack.push(7);
+miStack.push(1);
+console.log(miStack);
+
+console.log(miStack.pop());
+// console.log(miStack.pop());
+// console.log(miStack.pop());
+
+console.log(miStack.min());
+
+// const arr = [1, 2, 3];
+// Array.isArray(); // método de la CLASE ARRAY
+// arr.push(5); // método dinámico => pertenece a la INSTANCIA
--- Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
+++ Ejercicios/06-HasBalancedBrackets/HasBalancedBrackets.js
@@ -1,6 +1,47 @@
-function HasBalancedBrackets (string) {
-  // Your code here:
+function HasBalanceBrackets(string) {
+  if (string.length % 2 !== 0) return false;
 
+  // const openBrackets = ["(", "{", "["];
+  // const closeBrackets = [")", "}", "]"];
+  const validBrackets = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+  };
+
+  const openStack = [];
+
+  for (const bracket of string) {
+    if (validBrackets[bracket]) openStack.push(bracket);
+    else if (bracket !== validBrackets[openStack.pop()]) return false;
+  }
+  return !openStack.length; // 0 => false => true
 }
 
-module.exports = HasBalancedBrackets
+// recorrer un array > acceder a la prop de un objeto > acceder a la posición de un array
+// obj[prop] > arr[n] =>
+
+//  hashMap!!!!!!!!! VITAL PARA MI VIDA COMO PROGRAMADOR!!! VITALLL.. PERO VITAL EN SERIO, EH!
+// NO TAN VITAL... PERO MUY BUENA... ARRAY ASOCIATIVO...
+
+// // const index = closeBrackets.indexOf(bracket);
+// // const sister = openBrackets[index];
+// // if (openStack.pop() !== sister) return false;
+// if (openStack.length === 0) return true;
+// else return false;
+
+// return openStack.length === 0 ? true : false;
+
+module.exports = HasBalanceBrackets;
+
+// console.log(HasBalanceBrackets("{()}([[]])")); //(true)
+
+// console.log(HasBalanceBrackets("{[]()}")); //(true)
+
+// console.log(HasBalanceBrackets("{[(])}")); //(false)
+
+// console.log(HasBalanceBrackets("{[(")); //(false)
+
+// console.log(HasBalanceBrackets("{[([{()[]{}}])]}")); //(true)
+
+console.log(HasBalanceBrackets("{[]}}")); //(false)
